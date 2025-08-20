4bcb6ab780ac8e8d06f2d74e39a559267ca49535
Gael Emmanuel Valles Yañez <yanezgael576@gmail.com>
2025-08-19 23:43:18 -0600

    nuevo intento

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,27 +1,26 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Inicio from "./pages/inicio/inicioAdmin";
 import Register from "./pages/registro";
 import { AuthProvider } from "./context/AuthContext";
-
 import { ParticipantesProvider } from "./context/ParticipantesContext";
 import ProtectedRoutes from "./protectedRoutes";
 
 function App() {
   return (
     <AuthProvider>
-        <ParticipantesProvider>
-          {/* Aquí puedes agregar más contextos si es necesario */}
+      <ParticipantesProvider>
+        <BrowserRouter basename="/FrontRevolution">
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/registro" element={<Register />} />
-            <Route element={<ProtectedRoutes />} >
+            <Route element={<ProtectedRoutes />}>
               <Route path="/inicio" element={<Inicio />} />
- 
-            </Route>  
+            </Route>
           </Routes>
-        </ParticipantesProvider>
+        </BrowserRouter>
+      </ParticipantesProvider>
     </AuthProvider>
   );
 }
